b1d391f07932077f9774cbfa2d4799bb072a2eb0
SergioABC123 <orozcosergio297@gmail.com>
2026-07-07 17:13:22 -0600

    login, css, routes, controllers, dgraph

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import routes from './routes';
+import path from 'path';
 
 let app;
 
 export function createApp(){
     app= express(); 
-    
+
+    app.use(express.urlencoded({ extended: true })); // lo usamos para leer el formulario con el formato urlencoded
+    app.use(express.static(path.join(__dirname, '..', 'public'))); // carcar los estilos css
+
     app.use(routes);
 
     return app;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,25 +1,8 @@
 import { json, Router, Request, Response } from 'express';
+import pagesRouter from './pages';
 
 const router= Router(); // usamos el servicio de rutas de express
 
-router.get('', (req: Request, res: Response) => {
-    res.json({ message: 'raiz' });
-});
-
-router.get('/admin', (req: Request, res: Response) => { // Poder absoluto
-    res.json({ message: 'Hola admin' });
-});
-
-router.get('/moderator', (req: Request, res: Response) => { // no puede cambiar cosas en el servidor
-    res.json({ message: 'hola moderador' });
-});
-
-router.get('/groupLeader', (req: Request, res: Response) => { // usuario con permisos en comunidades especificas
-    res.json({ message: 'hola lider de grupo' });
-});
-
-router.get('/user', (req: Request, res: Response) => { // usuario normal
-    res.json({ message: 'hola usuario' });
-});
+router.use(pagesRouter);
 
 export default router;
